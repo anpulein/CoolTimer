04617ad3d0975dd33badec4631024f04383982ed
Dmitry <dimassdf5@fmail.com>
2021-11-15 17:52:04 +0300

    onCreating Settings

--- app/src/main/java/com/example/cooltimer/MainActivity.java
+++ app/src/main/java/com/example/cooltimer/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.cooltimer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
+import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
@@ -73,9 +80,14 @@ public class MainActivity extends AppCompatActivity {
 
                 @Override
                 public void onFinish() {
-                    MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(),
-                            R.raw.bell_sound);
-                    mediaPlayer.start();
+
+                    SharedPreferences sharedPreferences =
+                            PreferenceManager.getDefaultSharedPreferences(getApplicationContext()); // Получаем доступ к файлу
+                    if(sharedPreferences.getBoolean("enable_sound", true)) {
+                        MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(),
+                                R.raw.bell_sound);
+                        mediaPlayer.start();
+                    }
 
                     resetTimer();
                 }
@@ -100,5 +112,30 @@ public class MainActivity extends AppCompatActivity {
         int m = (progress / 60) % 60;
         int s = progress % 60;
         return String.format("%02d:%02d", m, s);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Creating menu
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.timer_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.action_settings) {
+            Intent openSettings = new Intent(this, SettingsActivity.class);
+            startActivity(openSettings);
+            return true;
+        } else if(id == R.id.action_about) {
+            Intent openAbout = new Intent(this, AboutActivity.class);
+            startActivity(openAbout);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
